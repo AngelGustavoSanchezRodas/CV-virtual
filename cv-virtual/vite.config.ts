25c33e115677e865e46518ca8abbdd7609083c35
Angel Gustavo Sanchez Rodas <asanchezr12@miumg.edu.gt>
2026-02-13 15:46:02 -0600

    cambios en el vite.config.ts

--- cv-virtual/vite.config.ts
+++ cv-virtual/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cv-virtual/',
+  // IMPORTANTE: El 'base' debe coincidir exactamente con el nombre de tu repo en GitHub
+  base: '/CV-virtual/',
   resolve: {
     alias: {
+      // Esto permite que el proyecto entienda los imports tipo '@/components/...'
       '@': path.resolve(__dirname, './src'),
     },
   },
